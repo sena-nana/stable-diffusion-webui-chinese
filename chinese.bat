bcda303e29f9ff3f4173378f9c215fd4a68d4dd8
Sena <34237511+sena-nana@users.noreply.github.com>
2022-10-12 15:40:04 +0800

    Add files via upload

---
+++ chinese.bat
@@ -0,0 +1,1 @@
+python chinese.py
---
+++ chinese.py
@@ -0,0 +1,16 @@
+import json
+with open("chinese.json",encoding = "utf-8") as f:
+    map=json.load(f)
+    print(f"汉化文件加载完成")
+for file,value in map.items():
+    print(f"正在汉化{file}")
+    hasvalue=len(value)
+    dovalue=0
+    with open(file,"r",encoding = "utf-8") as f:
+        code=f.read()
+    for old,new in value.items():
+        code.replace(old,new)
+        dovalue += 1
+    with open(file,"w",encoding = "utf-8") as f:
+        f.write(code)
+    print(f"{file}汉化完成，存在{hasvalue}条汉化条目，成功匹配{dovalue}")
